686093d7c5d9ab1e691631fd2f58e4e36ae6681a
Kyu Takahashi <skallcandy0@gmail.com>
2022-04-07 16:50:07 +0900

    fix: サーバー起動のコードのパッケージ名を変更

--- user-agent-reduction/main.go
+++ user-agent-reduction/main.go
@@ -4,20 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/hi-bridge-9/privacy-sandbox/lib/server"
 	"github.com/hi-bridge-9/privacy-sandbox/user-agent-reduction/ad_tech"
 	"github.com/hi-bridge-9/privacy-sandbox/user-agent-reduction/media"
-	"github.com/hi-bridge-9/privacy-sandbox/util"
 )
 
 var (
 	handlerFuncMap = map[string]func(w http.ResponseWriter, r *http.Request){
-		"/media":   media.Handler,
+		"/media":    media.Handler,
 		"/ad_tech/": ad_tech.Handler,
 	}
 )
 
 func main() {
-	s := util.NewWebServer(handlerFuncMap)
+	s := server.New(handlerFuncMap)
 	if err := s.Start("80"); err != nil {
 		log.Fatal(err)
 	}
